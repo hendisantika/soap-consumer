fd44d24091f9116e742ec9d2b00038721df6b687
hendisantika <hendisantika@yahoo.co.id>
2019-05-12 21:44:35 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/soapconsumer/SoapConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.soapconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapConsumerApplication.class, args);
+    }
+
+}
